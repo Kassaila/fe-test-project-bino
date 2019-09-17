6af8bcff4f7693a9d891d9f9ecf04dcb1cf5511f
dsymonov <dsymonov@justcoded.co>
2019-09-17 20:13:50 +0300

    Animation nav button

--- src/js/app.js
+++ src/js/app.js
@@ -3,18 +3,16 @@
 // This file will be compiled into app.js and will not be minified.
 // Feel free with using ES6 here.
 
-import dotsEffect from "./modules/dots";
-
 ($ => {
   // When DOM is ready
   $(() => {
     $(".nav__button_mobile").on("click", function() {
       if (!$(this).hasClass("active")) {
         $(this).addClass("active");
-        $('.nav__list').animate({opacity:'toggle', height: 'toggle'});
+        $(".nav__list").animate({ opacity: "toggle", height: "toggle" });
       } else {
         $(this).removeClass("active");
-        $('.nav__list').animate({opacity:'toggle', height: 'toggle'});
+        $(".nav__list").animate({ opacity: "toggle", height: "toggle" });
       }
     });
   });
