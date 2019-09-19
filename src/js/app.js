a253e038554bda697744b0e092f9c011b480eae0
dsymonov <dsymonov@justcoded.co>
2019-09-19 16:25:01 +0300

    Slider responsive

--- src/js/app.js
+++ src/js/app.js
@@ -3,11 +3,14 @@
 // This file will be compiled into app.js and will not be minified.
 // Feel free with using ES6 here.
 import navMobile from './modules/nav_mobile';
+import welcomeSlider from './modules/welcome_slider.js';
 
 ($ => {
   // When DOM is ready
   $(() => {
     // navigation mobile
     navMobile();
+    // welcome slider
+    welcomeSlider();
   });
 })(jQuery);
---
+++ src/js/modules/welcome_slider.js
@@ -0,0 +1,24 @@
+const welcomeSlider = () => {
+  if ($(".welcome_slider").length < 0) return;
+  $(".welcome_slider").slick({
+    infinite: false,
+    speed: 800,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><span class="icon-chevron-left"></span></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><span class="icon-chevron-right"></span></button>',
+    responsive: [
+      {
+        breakpoint: 980,
+        settings: {
+          arrows: false,
+          dots: true,
+          adaptiveHeight: true
+        }
+      }
+    ]
+  });
+};
+export default welcomeSlider;
