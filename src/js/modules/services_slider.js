2e1e1e6d9ec39e7d5be7137c13fa31e2f9813632
dsymonov <dsymonov@justcoded.co>
2019-09-23 16:09:27 +0300

    Delete arrow

--- src/js/modules/services_slider.js
+++ src/js/modules/services_slider.js
@@ -13,10 +13,6 @@ const servicesSlider = () => {
             vertical: true,
             verticalSwiping: true,
             dots: true,
-            prevArrow:
-                '<button type="button" class="slick-prev"><span class="icon-chevron-left"></span></button>',
-            nextArrow:
-                '<button type="button" class="slick-next"><span class="icon-chevron-right"></span></button>',
             responsive: [
                 {
                     breakpoint: 480,
